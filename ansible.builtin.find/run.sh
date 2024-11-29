84929d992f447391808e1328d5fb97b43184ac69
Kuzniatsou Yauheni <yauheni_kuzniatsou@epam.com>
2024-11-30 01:04:08 +0300

    Add ANSIBLE_CONFIG variable to run.sh

--- ansible.builtin.find/run.sh
+++ ansible.builtin.find/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-ansible-playbook find_files.yml --extra-vars "search_path=/home/eu21/AnsibleEu21/ansible.builtin.find"
-ansible-playbook find_files_comments.yml --extra-vars "search_path=/home/eu21/AnsibleEu21/ansible.builtin.find"
+
+export ANSIBLE_CONFIG=/home/eu21/AnsibleEu21/ansible.cfg
+
+ansible-playbook find_files.yml --extra-vars "search_path=/home/eu21/AnsibleEu21/ansible.builtin.find" -v
+# ansible-playbook find_files_comments.yml --extra-vars "search_path=/home/eu21/AnsibleEu21/ansible.builtin.find"
 
